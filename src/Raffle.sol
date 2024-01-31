f7d6a17e43c4be995626b60d34009809139ef67c
Gopal Chandak <141278896+gopalchandak-dev@users.noreply.github.com>
2024-02-01 02:58:53 +0530

    Our Commit

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,25 +1,3 @@
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// internal & private view & pure functions
-// external & public view & pure functions
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
